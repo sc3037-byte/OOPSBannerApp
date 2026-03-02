0a3347b15f652179f05dbb547f1ab72cfd953ea9
sc3037-byte <sc3037@srmist.edu.in>
2026-03-02 15:39:22 +0530

    Oopsbannerapp.java

--- Oopsbannerapp.java
+++ Oopsbannerapp.java
@@ -1,6 +1,17 @@
-public class Oopsbannerapp{
-	public static void main(String[] args){
-	    System.out.println("WELCOME TO OOPS");
-	    System.out.println("UC3");
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        String banner = String.join("\n",
+                "  *****     *****     *******    *****  ",
+                " *     *   *     *   *      *   *     * ",
+                " *     *   *     *   *      *   *       ",
+                " *     *   *     *    *******     *****  ",
+                " *     *   *     *   *              *    ",
+                " *     *   *     *   *        *     *    ",
+                "  *****     *****    *         *****     "
+        );
+
+        System.out.println(banner);
     }
 }
